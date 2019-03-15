e45f55fb0597a161ef535a4ed3c00c8cfda9cba2
chipueatfast <15520604@gm.uit.edu.vn>
2019-03-16 01:07:28 +0700

    bfins and afsuc

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 echo "running after success script"
 eval "$(ssh-agent -s)" # start ssh-agent cache
-chmod 600 .travis/id_rsa # allow read access to the private key
-ssh-add .travis/id_rsa # add the private key to ssh
+chmod 600 id_rsa_travis_grail # allow read access to the private key
+ssh-add id_rsa_travis_grail # add the private key to ssh
 
 git config --global push.default simple # push only one branch at a time
 git remote add deploy ssh://git@RE:$PORT$DEPLOY_DIR
