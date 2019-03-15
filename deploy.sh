b0be65c3b10697993d08c53cc8390555b2b8087a
chipueatfast <15520604@gm.uit.edu.vn>
2019-03-16 01:03:12 +0700

    edit openssl

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+echo "running after success script"
 eval "$(ssh-agent -s)" # start ssh-agent cache
 chmod 600 .travis/id_rsa # allow read access to the private key
 ssh-add .travis/id_rsa # add the private key to ssh
 
 git config --global push.default simple # push only one branch at a time
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git remote add deploy ssh://git@RE:$PORT$DEPLOY_DIR
 git push deploy master
 
 EOF
