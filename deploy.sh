c5e13a723f28f5ddb1da05ed67f7ed5230490809
chipueatfast <15520604@gm.uit.edu.vn>
2019-03-16 01:46:06 +0700

    debug travis

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ chmod 600 id_rsa_travis_grail # allow read access to the private key
 ssh-add id_rsa_travis_grail # add the private key to ssh
 
 git config --global push.default simple # push only one branch at a time
-git remote add deploy ssh://git@$IP$PORT$DEPLOY_DIR
+git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
