d44ca5da45cef2c3a4480041df14b9e8f33752e2
chipueatfast <15520604@gm.uit.edu.vn>
2019-03-16 01:32:29 +0700

    env var

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo $REMOTE_IP $REMOTE_IP
+echo $IP
 eval "$(ssh-agent -s)" # start ssh-agent cache
 chmod 600 id_rsa_travis_grail # allow read access to the private key
 ssh-add id_rsa_travis_grail # add the private key to ssh
 
 git config --global push.default simple # push only one branch at a time
-git remote add deploy ssh://git@RE:$REMOTE_IP$REMOTE_PORT$REMOTE_DEPLOY_DIR
+git remote add deploy ssh://git@$IP$PORT$DEPLOY_DIR
 git push deploy master
-
-EOF
