d429a4c7a000cd45fc3765624ce53bd08f64680d
chipueatfast <15520604@gm.uit.edu.vn>
2019-03-16 13:45:00 +0700

    qua nan roi

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ eval "$(ssh-agent -s)" # start ssh-agent cache
 chmod 600 id_rsa_travis_grail # allow read access to the private key
 ssh-add id_rsa_travis_grail # add the private key to ssh
 
-ssh root@$IP
+ssh -o StrictHostKeyChecking=no root@$IP
 #git config --global push.default simple # push only one branch at a time
 #git remote add deploy root@$IP:$DEPLOY_DIR
 #git push deploy master
