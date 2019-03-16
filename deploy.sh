876b9486bc2e9e3ec6b1477f895d80c12feb1cf7
chipueatfast <15520604@gm.uit.edu.vn>
2019-03-16 13:02:27 +0700

    test autobuild

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@ echo $IP
 eval "$(ssh-agent -s)" # start ssh-agent cache
 chmod 600 id_rsa_travis_grail # allow read access to the private key
 ssh-add id_rsa_travis_grail # add the private key to ssh
-ssh-keyscan $IP >> ~/.ssh/known_hosts
-echo "things went smoothly"
 git config --global push.default simple # push only one branch at a time
 git remote add deploy root@$IP:$DEPLOY_DIR
 git push deploy master
