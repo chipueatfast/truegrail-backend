9dae24c88416f948dd2cd09fe0bb53dcf434c401
chipueatfast <15520604@gm.uit.edu.vn>
2019-03-16 17:22:19 +0700

    try adding ssh

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,7 @@ eval "$(ssh-agent -s)" # start ssh-agent cache
 chmod 600 id_rsa_travis_grail # allow read access to the private key
 ssh-add id_rsa_travis_grail # add the private key to ssh
 
-
-echo $TRAVIS_SSH_KEY_TYPES ${TRAVIS_HOME}
-ssh root@$IP
-#git config --global push.default simple # push only one branch at a time
-#git remote add deploy root@$IP:$DEPLOY_DIR
-#git push deploy master
+#ssh  root@$IP
+git config --global push.default simple # push only one branch at a time
+git remote add -o StrictHostKeyChecking=no deploy root@$IP:$DEPLOY_DIR
+git push deploy master
