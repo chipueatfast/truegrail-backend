d5bd9f9d89c0229dd23b128ac353be0a9a05fc58
chipueatfast <15520604@gm.uit.edu.vn>
2019-03-16 17:58:07 +0700

    sua mot chut noi dung

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ chmod 600 id_rsa_travis_grail # allow read access to the private key
 ssh-add id_rsa_travis_grail # add the private key to ssh
 
 echo 'm' | ssh -o StrictHostKeyChecking=no root@$IP
+cat ~/.ssh/known_hosts
 git config --global push.default simple # push only one branch at a time
 git remote add deploy root@$IP:$DEPLOY_DIR
 git push deploy master
