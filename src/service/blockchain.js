87af8f06a109553609e45004eff045fdc216a7e6
chipueatfast <15520604@gm.uit.edu.vn>
2019-05-17 16:14:15 +0700

    add blockchain connect

--- src/service/blockchain.js
+++ src/service/blockchain.js
@@ -5,19 +5,14 @@ import Tx from 'ethereumjs-tx';
 
 const network = {
     development: 'ws://127.0.0.1:7545',
-    production: 'http://128.199.134.167:8546',
+    production: 'ws://128.199.134.167:8545',
 }
 
 class BlockchainService {
     web3;
     contractInstance;
     constructor() {
-        switch (process.env.NODE_ENV) {
-            case 'development': {
-                this.web3 = new Web3(network[process.env.NODE_ENV]);
-            }
-        }
-
+        this.web3 = new Web3(network[process.env.NODE_ENV]);
         this.contractInstance = new this.web3.eth.Contract(TrueGrailToken.abi, TrueGrailToken.address);
 
     }
