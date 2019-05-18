b8a05ea3a13c4e3aaabbf4c2e9856b795471d4d2
chipueatfast <15520604@gm.uit.edu.vn>
2019-05-18 09:41:34 +0700

    manually add changelog db to handle database versioningand syncing

--- src/sequelize/models/user.js
+++ src/sequelize/models/user.js
@@ -1,29 +1,29 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('user', {
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      passwordHash: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      refreshToken: DataTypes.STRING,
-      networkAddress: DataTypes.STRING,
-  }, {});
+    const User = sequelize.define('user', {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        passwordHash: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        refreshToken: DataTypes.STRING,
+        networkAddress: DataTypes.STRING,
+    }, {});
 
-  User.associate = function(models) {
-    // associations can be defined here
-  };
+    User.associate = function(models) {
+        // associations can be defined here
+    };
 
-  User.prototype.serialize = function () {
-      return {
-          ...this.dataValues,
-          passwordHash: undefined,
-      }
-  };
-  return User;
+    User.prototype.serialize = function () {
+        return {
+            ...this.dataValues,
+            passwordHash: undefined,
+        }
+    };
+    return User;
 };
