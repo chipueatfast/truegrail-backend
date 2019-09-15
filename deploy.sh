c5823f05fdcbab69e94cf7351bc567352f6617ba
chipueatfast <15520604@gm.uit.edu.vn>
2019-09-15 23:38:32 +0700

    remove echo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $IP
 eval "$(ssh-agent -s)" # start ssh-agent cache
 chmod 600 id_rsa_travis_grail # allow read access to the private key
 ssh-add id_rsa_travis_grail # add the private key to ssh
