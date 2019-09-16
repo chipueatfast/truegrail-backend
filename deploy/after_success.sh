cf304af702835e9d06dd75a98797164580493f61
chipueatfast <15520604@gm.uit.edu.vn>
2019-09-16 23:06:24 +0700

    run deploy right on travis

---
+++ deploy/after_success.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" # start ssh-agent cache
+chmod 600 id_rsa_travis_grail # allow read access to the private key
+ssh-add id_rsa_travis_grail # add the private key to ssh
+
+ssh -o StrictHostKeyChecking=no root@$IP:$DEPLOY_DIR 'bash -s' < ./ssh_instruction
+exit
+
---
+++ deploy/ssh_instruction.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git pull --ff >> ./deployment_log
+sudo kill $(lsof -t -i:2190) >> ./deployment_log
+yarn >> ./deployment_log
+yarn start & >> ./deployment_log
+exit
