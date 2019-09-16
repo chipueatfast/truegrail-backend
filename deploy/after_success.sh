4599d90ff4c39d71eec459b7fdd100c6c1897a96
chipueatfast <15520604@gm.uit.edu.vn>
2019-09-16 23:13:08 +0700

    run deploy right on travis

--- deploy/after_success.sh
+++ deploy/after_success.sh
@@ -4,6 +4,6 @@ eval "$(ssh-agent -s)" # start ssh-agent cache
 chmod 600 id_rsa_travis_grail # allow read access to the private key
 ssh-add id_rsa_travis_grail # add the private key to ssh
 
-ssh -o StrictHostKeyChecking=no root@$IP:$DEPLOY_DIR 'bash -s' < ./ssh_instruction
+ssh -o StrictHostKeyChecking=no root@$IP:$DEPLOY_DIR 'sudo sh' < ./deploy/ssh_instruction.sh
 exit
 
