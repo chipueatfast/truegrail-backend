1b9af269ef0df12ccdd63d1636d7cf936fc24fa1
chipueatfast <15520604@gm.uit.edu.vn>
2019-09-17 22:53:26 +0700

    print both standard and error into log

--- deploy/ssh_instruction.sh
+++ deploy/ssh_instruction.sh
@@ -5,6 +5,6 @@ eval "$(ssh-agent -s)" # start ssh-agent cache
 ssh-add ~/.ssh/deploy_key
 git pull --ff >> ./deployment_log
 sudo kill $(lsof -t -i:2190) >> ./deployment_log
-yarn >> ./deployment_log
-yarn start & >> ./deployment_log
+yarn &>> ./deployment_log
+yarn start & &>> ./deployment_log
 exit
