3adc14fb07890b4cc21be476e43b0fd873f96215
chipueatfast <15520604@gm.uit.edu.vn>
2019-09-17 23:15:19 +0700

    kill process too

--- deploy/ssh_instruction.sh
+++ deploy/ssh_instruction.sh
@@ -5,7 +5,7 @@ echo '------------------------' $(date) '-----------------------' >> ./deploymen
 eval "$(ssh-agent -s)" >> ./deployment_log 2>&1 # start ssh-agent cache
 ssh-add ~/.ssh/deploy_key >> ./deployment_log 2>&1
 git pull --ff >> ./deployment_log 2>&1
-sudo kill $(lsof -t -i:2190) >> ./deployment_log
+sudo kill $(lsof -t -i:2190) >> ./deployment_log 2>&1
 yarn >> ./deployment_log 2>&1
 yarn start &>> ./deployment_log 2>&1
 exit
