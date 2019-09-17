35147ef59070403d6113d88a7705b01c1050f85a
chipueatfast <15520604@gm.uit.edu.vn>
2019-09-17 23:30:32 +0700

    put yarn start inright place

--- deploy/ssh_instruction.sh
+++ deploy/ssh_instruction.sh
@@ -7,5 +7,5 @@ ssh-add ~/.ssh/deploy_key >> ./deployment_log 2>&1
 git pull --ff >> ./deployment_log 2>&1
 sudo kill $(lsof -t -i:2190) >> ./deployment_log 2>&1
 yarn >> ./deployment_log 2>&1
-yarn start &>> ./deployment_log 2>&1
+yarn start >> ./deployment_log 2>&1 &
 exit
