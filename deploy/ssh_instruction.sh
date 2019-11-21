474a14ca4c0ffb3c3b2b5ac5303bb96966788770
chipueatfast <15520604@gm.uit.edu.vn>
2019-11-21 23:31:39 +0700

    test the nvm in effect

--- deploy/ssh_instruction.sh
+++ deploy/ssh_instruction.sh
@@ -4,6 +4,7 @@ cd /usr/src/truegrail-backend
 echo '------------------------' $(date) '-----------------------' >> ./deployment_log
 eval "$(ssh-agent -s)" >> ./deployment_log 2>&1 # start ssh-agent cache
 nvm use 10.16.0
+node --version
 ssh-add ~/.ssh/truegrail_backend_deploy_key >> ./deployment_log 2>&1
 git pull --ff >> ./deployment_log 2>&1
 sudo kill $(lsof -t -i:2190) >> ./deployment_log 2>&1
