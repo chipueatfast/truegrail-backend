d8e001cb94339cc1a4b6363c1f2631638b2ce4c9
chipueatfast <15520604@gm.uit.edu.vn>
2019-11-22 21:38:21 +0700

    options in production

--- deploy/ssh_instruction.sh
+++ deploy/ssh_instruction.sh
@@ -9,6 +9,4 @@ node --version
 ssh-add ~/.ssh/truegrail_backend_deploy_key >> ./deployment_log 2>&1
 git pull --ff >> ./deployment_log 2>&1
 sudo kill $(lsof -t -i:2190) >> ./deployment_log 2>&1
-yarn >> ./deployment_log 2>&1
-yarn start >> ./deployment_log 2>&1 &
 exit
--- src/middleware/CORSPolicyGuard.js
+++ src/middleware/CORSPolicyGuard.js
@@ -1,5 +1,7 @@
+import { isProduction } from '~/util/environment';
+ 
 export default function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", isProduction() ? "http://128.199.134.167:82" : "*");
     res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS, PATCH")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     if (req.method === "OPTIONS" || req.method === "OPTIONS") {
