c264f607973981c07944b698c81603cd6ef6d6d0
chipueatfast <15520604@gm.uit.edu.vn>
2019-11-23 11:26:54 +0700

    update deploy

--- deploy/ssh_instruction.sh
+++ deploy/ssh_instruction.sh
@@ -3,9 +3,9 @@
 cd /usr/src/truegrail-backend
 echo '------------------------' $(date) '-----------------------' >> ./deployment_log
 eval "$(ssh-agent -s)" >> ./deployment_log 2>&1 # start ssh-agent cache
-node --version
 ssh-add ~/.ssh/truegrail_backend_deploy_key >> ./deployment_log 2>&1
 git pull --ff >> ./deployment_log 2>&1
+whoami
 bash -c 'forever stop 0'
 sudo kill $(lsof -t -i:2190) >> ./deployment_log 2>&1
 exit
